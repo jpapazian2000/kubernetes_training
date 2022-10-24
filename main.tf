d95170002b238faed258467d046bc429d41ee604
jerome <j_papazian2000@yahoo.fr>
2022-10-24 17:12:22 +0200

    with packer ...5

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "hcp_packer_image" "controller" {
     bucket_name = "k8s-controller-images"
     iteration_id = data.hcp_packer_iteration.gold.id
     cloud_provider = "gcp"
-    region = "europe-west9"
+    #region = "europe-west9"
 }
 
 data "tls_public_key" "controller" {
