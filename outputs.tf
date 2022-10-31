36be41119189dd95fd6c1cdc0b588186776bef18
jerome <j_papazian2000@yahoo.fr>
2022-10-31 10:31:42 +0100

    add worker output

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,9 @@ output "network_self_link" {
 output "controller_ip" {
     value = google_compute_instance.controller.*.network_interface.0.access_config.0.nat_ip
 }
+output "worker_ip" {
+    value = google_compute_instance.worker.*.network_interface.0.access_config.0.nat_ip
+}
 
 #output "worker_ip" {
 #    value = google_compute_instance.worker.*.network_interface.0.access_config.0.nat_ip
